a6f135c1c1a8883512a16c79fca661e80e896b09
phuongwj <phamjulia9@gmail.com>
2026-06-30 22:37:47 -0300

    renamed variable names

--- db.js
+++ db.js
@@ -4,11 +4,11 @@ dotenv.config();
 import mysql from 'mysql2/promise'
 
 const pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE
+    host: process.env.MYSQLHOST,
+    port: process.env.MYSQLPASSWORD,
+    user: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLDATABASE
 })
 
 export default pool;
